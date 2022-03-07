ccd7613a9dd3ab6e04ee2aa30e7f8016f5b6dbee
ji <jiyongprk@gmail.com>
2022-03-07 15:57:22 -0500

    added query selector for now. work on javascript after css is done

---
+++ main.js
@@ -0,0 +1,13 @@
+const booktitle = document.querySelector('.book-title');
+const bookAuthor = document.querySelector('.book-author');
+const bookPage = document.querySelector('.book-page');
+const bookReadCheckBox = document.querySelector('.book-read-checkbox');
+const addBookBtn= document.querySelector('.add-book-btn');
+
+
+const items = [{
+    title: "",
+    author: "",
+    page: "",
+    read: false
+}]
