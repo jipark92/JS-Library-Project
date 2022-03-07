4e6e28dd45f6e552321b6d0b4e84cc16cec7ccdf
ji <jiyongprk@gmail.com>
2022-03-07 17:59:12 -0500

    work on addbooktolibrary function

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-const booktitle = document.querySelector('.book-title');
+const bookTitle = document.querySelector('.book-title');
 const bookAuthor = document.querySelector('.book-author');
 const bookPage = document.querySelector('.book-page');
 const bookReadCheckBox = document.querySelector('.book-read-checkbox');
 const addBookBtn= document.querySelector('.add-book-btn');
+const readOrNot = document.querySelector('.read-or-not');
+const removeBookBtn = document.querySelector('remove-book');
 
-
-const items = [{
-    title: "",
-    author: "",
-    page: "",
+const books = [{
+    title: "test",
+    author: "test",
+    page: "test",
     read: false
 }]
+
+function addBookToLibrary () {
+    addBookBtn.addEventListener('click', () => {
+        if (!bookTitle.value || !bookAuthor.value || !bookPage.value){
+            console.log("no information submitted")
+        } else if (bookTitle.value && bookAuthor.value && bookPage.value){
+            console.log("information submitted!")
+        }
+    })
+};
+addBookToLibrary();
