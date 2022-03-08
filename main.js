2d50d9cad2200a8fdb5efcd670e61bc4228bcfae
ji <jiyongprk@gmail.com>
2022-03-08 17:03:21 -0500

    make constructor function and addBookToLibrary function to work!

--- main.js
+++ main.js
@@ -5,47 +5,44 @@ const bookReadCheckBox = document.querySelector('.book-read-checkbox');
 const addBookBtn= document.querySelector('.add-book-btn');
 const readOrNot = document.querySelector('.read-or-not');
 const removeBookBtn = document.querySelector('remove-book');
-//the objects
-let bookList = [{
-    title: "test",
-    author: "test",
-    page: "test",
-    read: false
-},
-]
+
+//the object
+let myLibrary = [];
 
 //the constructor
-function bookInfo(title,author,pages) {
+function Book(title,author,page) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.pages = page;
     // this.read = read;
     this.sayEverything = function () {
         console.log(title);
         console.log(author);
-        console.log(pages);
+        console.log(page);
         // console.log(read);
     }
 };
 
 
+
 //add book to library
 function addBookToLibrary () {
     addBookBtn.addEventListener('click', () => {
-        if (!bookTitle.value || !bookAuthor.value || !bookPage.value){
+        if (!bookTitle.value || !bookAuthor.value || !bookPage.value || !bookReadCheckBox){
             console.log("no information submitted")
-        } else if (bookTitle.value && bookAuthor.value && bookPage.value){
+        } else if (bookTitle.value && bookAuthor.value && bookPage.value && bookReadCheckBox){
             console.log("information submitted!")
             let submittedTitle = bookTitle.value;
             let submittedAuthor = bookAuthor.value;
             let submittedPage = bookPage.value;
             
-            console.log(submittedTitle);
-            console.log(submittedAuthor);
-            console.log(submittedPage);
+            // console.log(submittedTitle);
+            // console.log(submittedAuthor);
+            // console.log(submittedPage);
 
-            const books = new bookInfo(`${submittedTitle}`,`${submittedAuthor}`, `${submittedPage}`);
+            const books = new Book(`${submittedTitle}`,`${submittedAuthor}`, `${submittedPage}`);
             books.sayEverything();
+            // console.log(books.title +' test')
         }
     })
 };
@@ -57,23 +54,12 @@ addBookToLibrary()
 
 
 
-// const newBook = new library('harry', 'jk', '123')
-// newBook.sayEverything();
 
 
 
 
 
-//searches through bookList object/*array*
-function goThroughBooks(){
-    for (let i = 0; i < bookList.length; i++){
-        // console.log(bookList)//Array(3) [ {…}, {…}, {…} ]
-        // console.log(bookList[i]) //Object { title: "test", author: "test", page: "test", read: false }
-    }
-}
-goThroughBooks();
-
-//make it start with empty input because it keeps saving previous text
+//make it start with empty input because it keeps saving previous text after refresh
 window.addEventListener('DOMContentLoaded',()=>{
     bookTitle.value = "";
     bookAuthor.value = "";
