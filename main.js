494556b16b8ab85bc8c9d0e101a5c2c1e1a90426
ji <jiyongprk@gmail.com>
2022-03-09 18:44:58 -0500

    remove values from array working

--- main.js
+++ main.js
@@ -8,22 +8,15 @@ const bookInfoContainer = document.querySelector('.book-info-container');
 //the object
 let myLibrary = [];
 
-//make it start with empty input because it keeps saving previous text after refresh
-window.addEventListener('DOMContentLoaded',()=>{
-    bookTitle.value = "";
-    bookAuthor.value = "";
-    bookPage.value = "";
-    //make read checkbox unchecked later
-});
-
 //the constructor
-function Book(title,author,page) {
+function Book(title,author,page,read) {
     this.title = title;
     this.author = author;
     this.page = page;
-    // this.read = read;
-    myLibrary.push({title,author,page});
-    goThroughBooks(title,author,page);
+    this.read = read;
+    myLibrary.push({title,author,page,read});
+    console.log(myLibrary)
+    createHtml(title,author,page,read);
 };
 
 //add book to library
@@ -36,75 +29,136 @@ function addBookToLibrary () {
             let submittedTitle = bookTitle.value;
             let submittedAuthor = bookAuthor.value;
             let submittedPage = bookPage.value;
+            let submittedCheck = bookReadCheckBox.checked;
 
-            const books = new Book(`${submittedTitle}`,`${submittedAuthor}`, `${submittedPage}`);
+            new Book(`${submittedTitle}`,`${submittedAuthor}`, `${submittedPage}`, `${submittedCheck}`);
+            
         }
     })
 };
 addBookToLibrary();
 
 //go through array and add based on the array index to html display
-function goThroughBooks(title,author,page){
-    for (let i = 0; i < myLibrary.length; i++){
+function createHtml(title,author,page){
 
-            const addedBookInfo = document.createElement('div');
-            addedBookInfo.setAttribute('class', 'added-book-info');
-            bookInfoContainer.appendChild(addedBookInfo);
-            
-            const createPOne = document.createElement('p');
-            createPOne.setAttribute('class', 'title-text');
-            addedBookInfo.appendChild(createPOne);
-            createPOne.textContent = title;
+        
 
-            const createPTwo = document.createElement('p');
-            createPTwo.setAttribute('class', 'author-text');
-            addedBookInfo.appendChild(createPTwo);
-            createPTwo.textContent = author;
+        //create added book container
+        const addedBookInfo = document.createElement('div');
+        addedBookInfo.setAttribute('class', 'added-book-info');
+        bookInfoContainer.appendChild(addedBookInfo);
 
-            const createPThree = document.createElement('p');
-            createPThree.setAttribute('class', 'page-text');
-            addedBookInfo.appendChild(createPThree);
-            createPThree.textContent = page;
+        //set data attribute
+        for (let i = 0; i < myLibrary.length; i++){
+            addedBookInfo.setAttribute("id", myLibrary[i].title)
+        }
+        
+        //create title text display
+        const createPOne = document.createElement('p');
+        createPOne.setAttribute('class', 'title-text');
+        addedBookInfo.appendChild(createPOne);
+        createPOne.textContent = title;
 
-            const createPFour = document.createElement('p');
-            createPFour.setAttribute('class', 'read-text');
-            addedBookInfo.appendChild(createPFour);
+        //create author text display
+        const createPTwo = document.createElement('p');
+        createPTwo.setAttribute('class', 'author-text');
+        addedBookInfo.appendChild(createPTwo);
+        createPTwo.textContent = author;
+        
+        //create page number display
+        const createPThree = document.createElement('p');
+        createPThree.setAttribute('class', 'page-text');
+        addedBookInfo.appendChild(createPThree);
+        createPThree.textContent = page;
+
+        //create read status text html elements
+        const createPFour = document.createElement('p');
+        createPFour.setAttribute('class', 'read-text');
+        addedBookInfo.appendChild(createPFour);
+        
+        //create read status button
+        const readNotBtn = document.createElement('button');
+        readNotBtn.setAttribute('class', 'read-or-not');
+        addedBookInfo.appendChild(readNotBtn);
+        readNotBtn.textContent = "";
+
+        //create read status display to button and status
+        if (bookReadCheckBox.checked === true){
             createPFour.textContent = "Read";
-
-            const readNotBtn = document.createElement('button');
-            readNotBtn.setAttribute('class', 'read-or-not');
-            addedBookInfo.appendChild(readNotBtn);
             readNotBtn.textContent = "Not Read";
+        } else {
+            createPFour.textContent = "Not Read";
+            readNotBtn.textContent = "Read";
+        };
 
-            const removeBookBtns = document.createElement('button');
-            removeBookBtns.setAttribute('class', 'remove-book');
-            addedBookInfo.appendChild(removeBookBtns);
-            removeBookBtns.textContent = "Remove";
+        //create remove button
+        const removeBookBtns = document.createElement('button');
+        removeBookBtns.setAttribute('class', 'remove-book');
+        addedBookInfo.appendChild(removeBookBtns);
+        removeBookBtns.textContent = "Remove";
 
-            //toggle for read book or not read book status
+        //make the book buttons works
+        function bookButtons(){
+            //change read status
             readNotBtn.addEventListener('click', ()=>{
                 if (createPFour.textContent === "Read"){
                     createPFour.textContent = "Not Read"
                     readNotBtn.textContent = "Read";
                 } else {
                     createPFour.textContent = "Read"
-                    readNotBtn.textContent = "Not Read";
+                    readNotBtn.textContent = "Not Read"
                 }
             });
+
             
-            //remove add-added-book and all its children
+
+            // remove add-added-book and all its children
             removeBookBtns.addEventListener('click', ()=> {
+                //remove added book container
                 addedBookInfo.remove();
-                // myLibrary.unshift();
+                //removes object in array
+                // need to make ID and mylibrary.title connect then delete mylibrary object.
+                for (let i = 0; i < myLibrary.length; i++){
+                    let wee = addedBookInfo.getAttribute("id", myLibrary[i].title)
+                    console.log(wee);
+
+
+                    if(myLibrary[i].title === wee){
+                        delete myLibrary[i].title;
+                        delete myLibrary[i].author;
+                        delete myLibrary[i].page;
+                        delete myLibrary[i].read;
+
+
+                    }
+
+
+                    // let index = myLibrary.findIndex( element => {
+                    //     if (element.title = `${wee}`) {
+                    //       return true;
+                    //     }
+                    // });
+                    // console.log('title is at index: ' + index);
+                    
+                }
+
+
+
+
+
+
+
+
+              
             });
-
-
-
-        // console.log(myLibrary.length)
-        // console.log(myLibrary)//Array(3) [ {…}, {…}, {…} ]
-        // console.log(myLibrary[i]) //Object { title: "test", author: "test", page: "test", read: false }
-    }
+        }
+        bookButtons();
 };
 
-
-
+//make it start with empty input because it keeps saving previous text after refresh
+window.addEventListener('DOMContentLoaded',()=>{
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    bookPage.value = "";
+    //make read checkbox unchecked later
+});
